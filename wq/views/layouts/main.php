41c1d94c2784662f99fb2e9292ef813fcb38da32
WuQiang <188226814@qq.com>
2016-04-20 19:06:28 +0800

    Update

--- wq/views/layouts/main.php
+++ wq/views/layouts/main.php
@@ -30,6 +30,7 @@ $this->registerJs($js, View::POS_END);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
