5ef0810b8103fb3b83a9d01c7a40a31176e9de4f
WuQiang <188226814@qq.com>
2016-05-11 18:36:15 +0800

    便签页面布局完成

---
+++ wq/views/note/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$this->params['pageId'] = 'note-page'
+?>
+<ul class="note-blocks">
+    <li>
+        RI&E
+        <div class="popup">
+            <p>
+                设置文字不换行，超过的部分用“...”代替<br>
+                overflow: hidden;<br>
+                text-overflow: ellipsis;<br>
+                white-space: nowrap;<br>
+                width: 210px;<br>
+                除width属性外，顺序不要颠倒哦<br>
+                设置文字不换行，超过的部分用“...”代替<br>
+                overflow: hidden;<br>
+                text-overflow: ellipsis;<br>
+                white-space: nowrap;<br>
+                width: 210px;<br>
+                除width属性外，顺序不要颠倒哦<br>
+            </p>
+        </div>
+    </li>
+</ul>
