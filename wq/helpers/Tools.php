1aa94e1c83a7fe52f202ea5848f0d1a3106d8769
WuQiang <188226814@qq.com>
2016-06-22 14:53:58 +0800

    Update

--- wq/helpers/Tools.php
+++ wq/helpers/Tools.php
@@ -116,11 +116,139 @@ class Tools
 
     public static function dev($content, $end = 0)
     {
+        echo '<pre>';
         print_r($content);
-        echo '<br>';
-        if($end) {
+        echo '</pre>';
+        if($end == 0) {
             exit;
         }
+    }
+
+    public static function convert($num, $adv, $number='')
+    {
+        if($num >= 0) {
+            $arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z',
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z',
+            ];
+            $rem = $num % $adv;
+            $number = $arr[$rem] . $number;
+            $int = floor($num / $adv);
+            if ($int >= $adv) {
+                $number = self::convert($int, $adv, $number);
+            } elseif ($int > 0) {
+                $number = $arr[$int] . $number;
+            }
+        }else {
+            $num = str_replace("-", "", $num);
+            $number = '-'.self::convert($num, $adv, $number);
+        }
+        return $number;
+    }
+
+    /*
+     * 冒泡排序
+     */
+    public static function bubbleSort($arr)
+    {
+        $len=count($arr);
+        //该层循环控制 需要冒泡的轮数
+        for($i=1;$i<$len;$i++)
+        { //该层循环用来控制每轮 冒出一个数 需要比较的次数
+            for($k=0;$k<$len-$i;$k++)
+            {
+                if($arr[$k]>$arr[$k+1])
+                {
+                    $tmp=$arr[$k+1];
+                    $arr[$k+1]=$arr[$k];
+                    $arr[$k]=$tmp;
+                }
+            }
+        }
+        return $arr;
+    }
+
+    /*
+     * 选择排序
+     */
+    public static function selectSort($arr) {
+        //双重循环完成，外层控制轮数，内层控制比较次数
+        $len=count($arr);
+        for($i=0; $i<$len-1; $i++) {
+            //先假设最小的值的位置
+            $p = $i;
+
+            for($j=$i+1; $j<$len; $j++) {
+                //$arr[$p] 是当前已知的最小值
+                if($arr[$p] > $arr[$j]) {
+                    //比较，发现更小的,记录下最小值的位置；并且在下次比较时采用已知的最小值进行比较。
+                    $p = $j;
+                }
+            }
+            //已经确定了当前的最小值的位置，保存到$p中。如果发现最小值的位置与当前假设的位置$i不同，则位置互换即可。
+            if($p != $i) {
+                $tmp = $arr[$p];
+                $arr[$p] = $arr[$i];
+                $arr[$i] = $tmp;
+            }
+        }
+        //返回最终结果
+        return $arr;
+    }
+
+    /*
+     * 插入排序
+     */
+    public static function insertSort($arr) {
+        $len=count($arr);
+        for($i=1; $i<$len; $i++) {
+            $tmp = $arr[$i];
+            //内层循环控制，比较并插入
+            for($j=$i-1;$j>=0;$j--) {
+                if($tmp < $arr[$j]) {
+                    //发现插入的元素要小，交换位置，将后边的元素与前面的元素互换
+                    $arr[$j+1] = $arr[$j];
+                    $arr[$j] = $tmp;
+                } else {
+                    //如果碰到不需要移动的元素，由于是已经排序好是数组，则前面的就不需要再次比较了。
+                    break;
+                }
+            }
+        }
+        return $arr;
+    }
+
+    /*
+     * 快速排序
+     */
+    public static function quickSort($arr) {
+        //先判断是否需要继续进行
+        $length = count($arr);
+        if($length <= 1) {
+            return $arr;
+        }
+        //选择第一个元素作为基准
+        $base_num = $arr[0];
+        //遍历除了标尺外的所有元素，按照大小关系放入两个数组内
+        //初始化两个数组
+        $left_array = array();  //小于基准的
+        $right_array = array();  //大于基准的
+        for($i=1; $i<$length; $i++) {
+            if($base_num > $arr[$i]) {
+                //放入左边数组
+                $left_array[] = $arr[$i];
+            } else {
+                //放入右边
+                $right_array[] = $arr[$i];
+            }
+        }
+        //再分别对左边和右边的数组进行相同的排序处理方式递归调用这个函数
+        $left_array = self::quicksort($left_array);
+        $right_array = self::quicksort($right_array);
+        //合并
+        return array_merge($left_array, array($base_num), $right_array);
     }
 }
 
--- wq/views/article/test.php
+++ wq/views/article/test.php
@@ -9,9 +9,31 @@ use app\helpers\Tools;
         <div style="background: #9cb2dd;border-radius:3px;">
             <h3>语言切换：<small>配置在common.php</small></h3>
             <?php
-            Tools::dev(Yii::t('yii','Update'));
-            Tools::dev(Yii::t('cn2', 'cn'));
-            Tools::dev(Yii::t('cn4', 'vi', [], 'vi'));
+            Tools::dev(Yii::t('yii','Update'), 1);
+            Tools::dev(Yii::t('cn2', 'cn'), 1);
+            Tools::dev(Yii::t('cn4', 'vi', [], 'vi'), 1);
+            ?>
+        </div>
+        <div style="background: #96ddb8;border-radius:3px;">
+            <h3>十进制换算为其他（2-62）进制：</h3>
+            <?php
+            Tools::dev(Tools::convert(312312312,32), 1);
+            Tools::dev(Tools::convert(31123,2), 1);
+            Tools::dev(Tools::convert(42342,16), 1);
+            ?>
+        </div>
+        <div style="background: #ddb39e;border-radius:3px;">
+            <h3>PHP数组排序算法：</h3>
+            <?php
+            $arr = [1,43,54,62,21];
+            echo "<h4>冒泡排序：</h4>";
+            Tools::dev(Tools::bubbleSort($arr), 1);
+            echo "<h4>选择排序：</h4>";
+            Tools::dev(Tools::selectSort($arr), 1);
+            echo "<h4>插入排序：</h4>";
+            Tools::dev(Tools::insertSort($arr), 1);
+            echo "<h4>快速排序：</h4>";
+            Tools::dev(Tools::quickSort($arr), 1);
             ?>
         </div>
     </body>
