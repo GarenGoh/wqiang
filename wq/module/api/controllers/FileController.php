733633f8249211882dd4d45316dd21dd3e2c30b1
Garen.Goh <188226814@qq.com>
2016-10-10 15:35:39 +0800

    Update

--- wq/module/api/controllers/FileController.php
+++ wq/module/api/controllers/FileController.php
@@ -17,14 +17,14 @@ class FileController extends Controller
             $prefix = Yii::$app->request->getQueryParam('prefix', 'default');
             $name = Yii::$app->request->getQueryParam('name', 'file');
             $uploadedFile = UploadedFile::getInstanceByName($name);//要被上传的文件
-            $newName = Tools::getRandChar(8). '.' . $uploadedFile->extension;
+            $newName = Tools::getRandChar(8).'.'.$uploadedFile->extension;
             $options = [
                 'prefix' => $prefix.'/',
                 'newName' => $newName
             ];
 
-            if ($uploadedFile) {
-                $result = $uploadedFile->saveAs('images/'.$prefix.'/'.$newName );//上传文件
+            if($uploadedFile) {
+                $result = $uploadedFile->saveAs('images/'.$prefix.'/'.$newName);//上传文件
                 if($result) {
                     //缩略图
                     //Tools::makeThumbnail('images/'.$newName,'minimages/'.$newName,'80','70');
@@ -41,7 +41,7 @@ class FileController extends Controller
         $id = Yii::$app->request->getBodyParam('id');
         if($_FILES) {
             $uploadedFile = UploadedFile::getInstanceByName('image');//获取要被上传的文件; image 是在 Simditor 上传组件的 fileKey 参数
-            if($uploadedFile instanceof UploadedFile){
+            if($uploadedFile instanceof UploadedFile) {
                 return Yii::$app->fileService->saveToQiNiu($uploadedFile, $id);
             }
             return ['success' => false, 'msg' => '没有得到指定的文件!'];
