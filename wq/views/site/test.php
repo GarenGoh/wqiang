a396c63a3913fd2d33fccc4d6980fcb7a7210045
Garen.Goh <188226814@qq.com>
2016-11-01 17:13:26 +0800

    Update

--- wq/views/site/test.php
+++ wq/views/site/test.php
@@ -4,18 +4,18 @@
     <source  src="/music/tkzc.mp3" type="audio/mp3">
 </audio>
 <p class="note" style="background-color: grey">11111</p>
-
 <?php
 $js = '
     $(".note").mouseenter(function(){
       $(".note").css("background-color","red");
-      $(".music").attr("autoplay","autoplay");
       $(".music")[0].play()
     }).mouseleave(function(){
       $(".note").css("background-color","grey");
       $(".music")[0].pause();
     });
+    console.log($(".music"));
 ';
-//
+//$(".music")[0].currentTime = 0;  //设置当前时间
+//$(".music")[0].duration   //该音乐的总时长
 $this->registerJs($js,\yii\web\View::POS_END);
 ?>
